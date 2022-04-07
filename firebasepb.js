c5d4f91df57fb5ca2fa8da2af3337159c2ecb08f
boris calli <201900823@est.umss.edu>
2022-04-06 23:50:31 -0400

    prueba de subida de gif

--- firebasepb.js
+++ firebasepb.js
@@ -1,9 +1,11 @@
 // Import the functions you need from the SDKs you need
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+
+import { getFirestore, collection, addDoc, doc, setDoc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";    
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
-
   // Your web app's Firebase configuration
   const firebaseConfig = {
     apiKey: "AIzaSyAaV8Dh_vmrznM9_XxzmYZbl35UUIzueGI",
@@ -16,7 +18,49 @@ import { getFirestore, collection, addDoc } from "https://www.gstatic.com/fireba
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const db = getFirestore();
-export const savepb = (nombre) => {
-  addDoc(collection(db, 'prueba'), {nombre})
-}
+
+
+
+  /*const db = getFirestore();
+  const storage = getStorage();
+
+  function getName(fileimg){
+    var temp = fileimg.name.split('.');
+    const name = temp.slice(0,-1).join('.');
+
+    temp = fileimg.name.split('.');
+    const extName = temp.slice((temp.length-1),(temp.length));
+    return name+'.'+extName[0];
+  }
+export const savepb = (nombre,fileimg) => {
+  async function UploadPrecess(){
+    var imgToUpload = fileimg;
+    var imgname = getName(fileimg);
+    const metaData = {
+      contentType : imgToUpload.type
+    }
+    const storageRef = ref(storage, "/images"+imgname);
+    const UploadTask = uploadBytesResumable(storageRef, imgToUpload, metaData);
+    UploadTask.on('state-changed',(snapshot)=>{
+
+    },
+    (error)=>{
+      alert("error: imagen no subida");
+    },
+    ()=>{
+      getDownloadURL(UploadTask.snapshot.ref).then((downloadURL)=>{
+        guardarURL(downloadURL);
+      });
+    }
+    );
+  }
+  var url = "";
+  async function guardarURL(downloadURL){
+    var sref = doc(db, "pruebas/");
+    await setDoc(sref,{
+      NOMBREIMAGE: nombre, 
+      IMAGEURL: downloadURL
+    })
+  }
+  //addDoc(collection(db, 'prueba'), {nombre, url})
+}*/
--- pb.js
+++ pb.js
@@ -1,13 +1,60 @@
-import { savepb } from "./firebasepb.js";
-window.addEventListener('DOMContentLoaded', ()=>{
+import {  } from "./firebasepb.js";
 
+const form = document.getElementById('registro');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const nombreEjercicio = form ['nombre ejercicio']
+    const img = form ['formGif']
+    console.log(nombreEjercicio.value,img.value)
+    console.log(img.value.split(/(\\|\/)/g).pop())
+    var fl = []
+    fl = (img.value.split(/(\\|\/)/g).pop()).split('.')
+    
+    const imgnombre = fl[0]
+    const imgext = fl[1]
+    console.log(imgnombre,imgext)
 })
 
-const forml = document.getElementById('registro');
+/*window.addEventListener('DOMContentLoaded', ()=>{
 
-forml.addEventListener('submit', (e) =>{
+})*/
+
+
+
+/*const forml = document.getElementById('registro');
+const img = [];
+//const reader = new FileReader();
+//const input = document.createElement('input');
+//input.type = 'file';
+const nameimg = ""; 
+const extimg = "";
+
+
+/*input.onchange = e =>{
+    files = e.target.files;
+    reader.readAsDataURL(files[0]);
+}
+
+reader.onload = function(){
+
+}*/
+
+/*function GetFileName(imgfile){
+    var = imgfile.name.
+}
+function GetExtName(imgfile){
+
+}*/
+
+/*forml.addEventListener('submit', (e) =>{
     e.preventDefault()
     const nombre = forml ['nombre ejercicio']
-    savepb(nombre.value)
-    console.log(nombre)
+    img = forml ['formGif']
+    //nameimg = GetFileName(img[0]);
+    //extimg = GetExtName(img[0]);
+
+    console.log(img.value)
+    savepb(nombre.value,img[0])
 })
+*/
