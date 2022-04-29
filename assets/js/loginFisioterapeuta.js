30f357951c4a8133ca784123f3a0886371317f49
boris calli <201900823@est.umss.edu>
2022-04-29 01:10:05 -0400

    cambios

--- assets/js/loginFisioterapeuta.js
+++ assets/js/loginFisioterapeuta.js
@@ -1,21 +1,4 @@
 import { autenticacion } from './firebase.js'
-let con=true;
-function verClave(){
-  let ver=document.getElementById("ver");
-  let clave=document.getElementById("contraseñaFisioterapeuta")
-  let icono=document.getElementById("icono")
-  if (con==true) {
-    clave.type="text"
-    icono.classList.add("fa-eye-slash")
-    con=false
-}else{
-    clave.type="password"
-    icono.classList.remove("fa-eye-slash")
-    con=true
-    }
-}
-
-
 
 const login = document.getElementById('login');
 
@@ -24,7 +7,7 @@ login.addEventListener('submit', function (e) {
   const email = document.getElementById('emailFisioterapeuta');
   const pass = document.getElementById('contraseñaFisioterapeuta');
   autenticacion(email.value,pass.value);
-
+  
 });
 
 
